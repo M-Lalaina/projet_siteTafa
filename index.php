17c4b00ea551286140a9b63d5e9a31b09292674d
Anjamanuellah <manuellahanja@gmail.com>
2022-10-05 10:12:43 +0300

    add styles

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
-    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="assets/styles/style.css">
     <title>Inscription tafa </title>
 </head>
 <body>
